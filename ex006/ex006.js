89a20e62b7ac4c3c91d21362ab94d5dfcb0e1827
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-06-15 21:53:07 -0300

    aaa

---
+++ ex006/ex006.js
@@ -0,0 +1,10 @@
+function clicar() {
+    var area = window.document.getElementById ("acao")
+    area.innerText = "Clicou!"
+}
+
+function passar() {
+    var area1 = window.document.getElementById ("enter")
+    area1.innerText = "Entrou"
+    
+}
