720cdfa5a4a85cbbb8b7c4ab1d176f66e2bda778
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-07-06 19:50:15 -0300

    imagens

--- projetos/relogio/relogio.js
+++ projetos/relogio/relogio.js
@@ -1,18 +1,18 @@
 
 function carregar() {
     var msg = document.getElementById ("mensagem")
-    var imagem= document.getElementById("imagem")
+    var img = document.getElementById("imagem")
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = "Agora são " +hora+ " horas."
-    if (hora >= 0 && hora < 12) {
-        img.src = "manha.jpg"
-    }
-    
-    else if (hora >= 12 && hora < 18) {
-        img.src = "tarde.jpg"
+    if (hora > 0 && hora < 12) {
+        img.src = "imagens/manha.jpg"
+
+    } else if (hora > 12 && hora < 18) {
+        img.src = "imagens/tarde.jpg"
     }
     else {
-        imag.src = "noite.jpg"
+        img.src = "imagens/noite.jpg"
+        document.body.style.backgroundColor = "#121238"
     }
 }
