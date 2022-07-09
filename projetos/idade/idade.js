3d67126396b5f6897f913d038c470c9015e75b2b
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-07-08 23:34:50 -0300

    aaa

--- projetos/idade/idade.js
+++ projetos/idade/idade.js
@@ -4,41 +4,35 @@ function clicar() {
     var datanas = document.getElementById ("databox")
     var res = document.getElementById ("resposta")
     var sex = document.getElementsByName ("radsex")
-    var idade = ano - datanas.value
-    var genero = ""
+    
     if (datanas.value.lenght > ano || datanas.value == 0) {
         window.alert ("Verifique os dados e tente novamente!")
     } else {
+        var idade = ano - datanas.value
+        var genero = ""
         res.style.textAlign = "center"
         var img = document.createElement ("img")
+        
         if (sex[0].checked){
             genero = "Homem"
-            if (idade >0 && idade < 12) {
-                //criança
-                img.setAttribute ("src", "menino.png")
+            if (idade >=0 && idade < 12) {
+                img.setAttribute ("src", "criancamas.png")
             } else if (idade < 20) {
-                // jovem
                 img.setAttribute ("src", "homemjovem.png")
             } else if (idade < 60) {
-                //adulto
                 img.setAttribute ("src", "homemadulto.png")
             } else {
-                //idoso
                 img.setAttribute ("src", "idoso.png")
             }
-        } else if (sex[1].checked) {
-            genero ="mulher"
-            if (idade >0 && idade < 12) {
-                //criança
-                img.setAttribute ("src", "menina.png")
+        } else if (sex[1].checked){
+            genero = "mulher"
+            } if (idade >=0 && idade < 12) {
+                img.setAttribute ("src", "criancafem.png")
             } else if (idade < 20) {
-                // jovem
                 img.setAttribute ("src", "jovemmulher.png")
             } else if (idade < 60) {
-                //adulto
                 img.setAttribute ("src", "mulher.png")
             } else {
-                //idoso//
                 img.setAttribute ("src", "idosa.png")
             }
         }
@@ -46,4 +40,3 @@ function clicar() {
         res.appendChild(img)
     }
    
-}
