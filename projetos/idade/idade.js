9f5786e920ad8ca0e85e728943035bf402745746
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-07-08 23:10:04 -0300

    bug imagem

--- projetos/idade/idade.js
+++ projetos/idade/idade.js
@@ -9,15 +9,41 @@ function clicar() {
     if (datanas.value.lenght > ano || datanas.value == 0) {
         window.alert ("Verifique os dados e tente novamente!")
     } else {
-        
+        res.style.textAlign = "center"
+        var img = document.createElement ("img")
         if (sex[0].checked){
             genero = "Homem"
+            if (idade >0 && idade < 12) {
+                //criança
+                img.setAttribute ("src", "menino.png")
+            } else if (idade < 20) {
+                // jovem
+                img.setAttribute ("src", "homemjovem.png")
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute ("src", "homemadulto.png")
+            } else {
+                //idoso
+                img.setAttribute ("src", "idoso.png")
+            }
         } else if (sex[1].checked) {
             genero ="mulher"
+            if (idade >0 && idade < 12) {
+                //criança
+                img.setAttribute ("src", "menina.png")
+            } else if (idade < 20) {
+                // jovem
+                img.setAttribute ("src", "jovemmulher.png")
+            } else if (idade < 60) {
+                //adulto
+                img.setAttribute ("src", "mulher.png")
+            } else {
+                //idoso//
+                img.setAttribute ("src", "idosa.png")
+            }
         }
         res.innerHTML = ("Você é " +genero+ " com " +idade+ " anos.")
+        res.appendChild(img)
     }
-    switch {
-        
-    }
+   
 }
