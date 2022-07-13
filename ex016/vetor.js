97f9ceaa2baa1ca911c165ee68136ca55b70efdb
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-07-13 19:48:49 -0300

    fim ex016

---
+++ ex016/vetor.js
@@ -0,0 +1,11 @@
+var num = [5, 6, 7, 3, 8, 9]   
+
+/*for (pos = 0; pos <= num.length; pos++) {
+    console.log(num[pos])
+} */
+
+for (var pos in num) {
+    num.sort()
+    console.log (num[pos]) 
+   
+}
