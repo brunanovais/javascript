82de914d687410c2cf99e799de09d044c3a44d75
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-07-19 21:53:36 -0300

    TA FLUINDO

--- ex017/modelo.js
+++ ex017/modelo.js
@@ -1,4 +1,14 @@
 function adcionar(){
-    var num = document.getElementById("numbox")
-    num = Number(numbox)
+    var numbox1 = document.getElementById("numbox")
+    var num = Number(numbox1.value) 
+    
+    if (num == "") {
+         window.alert('[ERRO] Digite um numero.')
+    } else {
+        var lista = document.getElementById("sel")
+        lista.options[lista.options.length] = new Option(+num+ " foi adcionado");
+        //lista.innerText=("Você adcionou " +num)
+        //window.alert(num)
+    }
+   
 }
