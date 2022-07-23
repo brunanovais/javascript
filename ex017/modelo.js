329bc3f1138105a21cfcb69a9434545d21294f3e
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-07-22 21:32:38 -0300

    aaaaa

--- ex017/modelo.js
+++ ex017/modelo.js
@@ -1,14 +1,23 @@
-function adcionar(){
-    var numbox1 = document.getElementById("numbox")
-    var num = Number(numbox1.value) 
-    
+var numbox1 = document.getElementById("numbox")
+var num = Number(numbox1.value) 
+var vetor = [num]
+
+function adcionar() {
+    vetor = [num]
+    num = Number(numbox1.value) 
     if (num == "") {
-         window.alert('[ERRO] Digite um numero.')
+         window.alert('[ERRO] Digite um numero!')
     } else {
         var lista = document.getElementById("sel")
         lista.options[lista.options.length] = new Option(+num+ " foi adcionado");
-        //lista.innerText=("Você adcionou " +num)
-        //window.alert(num)
+        vetor.push(num)
     }
    
+}
+
+function finalizar (){
+    num = Number(numbox1.value) 
+    var res = document.getElementById("resposta")
+    res.innerText = (vetor.sort())
+    
 }
