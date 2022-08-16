d0a33a76f338412f855342fa5a1b6d79858cdbcf
brunanovais <93659717+brunanovais@users.noreply.github.com>
2022-08-15 22:34:24 -0300

    a

--- ex015/modelo.js
+++ ex015/modelo.js
@@ -4,14 +4,26 @@ function clicar(){
     if (n.value.length == 0){
         window.alert ("Por favor, digite um número!")
     } else {
-        var n = Number(numero.value)
-        s.innerHTML = ""
-        for (var c = 1; c <= 10; n * c) {
-            var item = document.createElement("option")
-            item.text = n+ "x" +c+ "=" +n*c
-            item.value = "tab"+c
-            s.appendChild(item)
-            c++
-        }
+       n = Number(numero.value)
+       montar_tabela(n, s)
     }
 }
+
+function montar_tabela(numero_usuario, select_tabuada) {
+    
+    select_tabuada.innerHTML = ""
+    for (var c = 1; c <= 10; numero_usuario * c) {
+        var item = document.createElement("option")
+        item.text = numero_usuario+ "x" +c+ "=" +produto(numero_usuario, c)
+        item.value = "tab"+c
+        select_tabuada.appendChild(item)
+        c++
+    }
+
+}
+
+function produto(multiplicador, multiplicando){
+
+    return multiplicador*multiplicando
+}
+
